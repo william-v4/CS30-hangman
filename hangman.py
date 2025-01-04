c29d0645e7c4132dcb68a2c5896ba641cf0a03aa
William Zhang <77857686+william-v4@users.noreply.github.com>
2025-01-04 14:10:50 -0700

    title

--- hangman.py
+++ hangman.py
@@ -1,7 +1,37 @@
+import os
 import random
-from time import sleep
+import customtkinter as gui
+from tkinter import *
+from tkinter.font import Font
 
+# enable brainrot words
 brainrot = True
+
+# load fonts
+gui.FontManager.load_font(os.path.join("Poppins-SemiBold.ttf"))
+gui.FontManager.load_font(os.path.join("Roboto-Regular.ttf"))
+# set text styles
+displaytext = ("Poppins", 32)
+normaltext = ("Roboto", 16)
+titletext = ("Roboto", 24) 
+# gui window
+window = gui.CTk()
+# window title
+window.title("Hangman")
+
+# clear the window
+def clear():
+    # for every widget in the window
+    for widget in window.winfo_children():
+        # eject them
+        widget.destroy()
+
+# create label
+def label(text : str, style : tuple, row : int, column : int, columnspan=None):
+    if columnspan:
+        gui.CTkLabel(window, text=text, font=style).grid(row=row, column=column, columnspan=columnspan, padx=10, pady=10)
+    else:
+        gui.CTkLabel(window, text=text, font=style).grid(row=row, column=column, padx=10, pady=10)
 
 # get random word randomly either from common or brainrot words file
 def getword() -> str:
@@ -14,6 +44,19 @@ def getword() -> str:
     # get a random word from the word list, strip it of whitespace and newline characters, make it lowercase, and return it
     return random.choice(wordlist).replace("\n", "").replace(" ", "").lower()
 
-while True:
-    print(getword())
-    sleep(1)
+# prompt for game parameters
+def gameconfig():
+    # clear the window
+    clear()
+    # create and place title label
+    label("Welcome to Hangman", titletext, 1, 1, columnspan=2)
+    # parameter labels
+
+
+"""
+MAIN THREAD
+"""
+# ask for initial parameters
+gameconfig()
+# start window
+window.mainloop()
